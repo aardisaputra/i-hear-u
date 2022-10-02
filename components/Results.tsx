ffa0a52b0ac6e2f31ed283714d250ca1e0b0de28
Cheng-Han Chuang <andrewchuang@berkeley.edu>
2022-10-01 23:41:46 -0400

    added top artists and songs; changed id page

--- components/Results.tsx
+++ components/Results.tsx
@@ -35,6 +35,35 @@ const rows = [
   createData('5th Place', "Funny", 16.0, 49),
 ];
 
+function creatSongs(
+  rank: string,
+  song: string,
+  artist: string,
+  time: number,
+) {
+  return {rank, song, artist, time};
+}
+
+const rowsSongs = [
+  creatSongs('1st Place', "Call Me", "bah", 24),
+  creatSongs('2nd Place', "Fuck Me", "bah", 37),
+  creatSongs('3rd Place', "Eat Me", "bah", 24),
+];
+
+function creatArt(
+  rank: string,
+  song: string,
+  time: number,
+) {
+  return {rank, song, time};
+}
+
+const rowsArt = [
+  creatSongs('1st Place', "Call Me", "bah", 24),
+  creatSongs('2nd Place', "Fuck Me", "bah", 37),
+  creatSongs('3rd Place', "Eat Me", "bah", 24),
+];
+
 // export default function BasicTable() {
 //   return (
     
@@ -85,7 +114,7 @@ const Home: NextPage = () => {
           alignItems="center"
           minHeight="8vh"
         ></Box>
-        <h1 className={styles.title}>Top 3 Fav Artists from Y'all!</h1>
+        <h1 className={styles.title}>Top 3 Songs for you 🎶</h1>
         <Box
           display="flex"
           justifyContent="center"
@@ -97,13 +126,13 @@ const Home: NextPage = () => {
             <TableHead>
               <TableRow>
                 <TableCell>Ranking</TableCell>
-                <TableCell align="left">Name</TableCell>
-                <TableCell align="right">Stats</TableCell>
-                <TableCell align="right">Total Score</TableCell>
+                <TableCell align="left">Song</TableCell>
+                <TableCell align="right">Artist</TableCell>
+                <TableCell align="right">Time Listened</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
+              {rowsSongs.map((row) => (
                 <TableRow
                   key={row.rank}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -111,9 +140,47 @@ const Home: NextPage = () => {
                   <TableCell component="th" scope="row">
                     {row.rank}
                   </TableCell>
-                  <TableCell align="left">{row.name}</TableCell>
-                  <TableCell align="right">{row.qs}</TableCell>
-                  <TableCell align="right">{row.score}</TableCell>
+                  <TableCell align="left">{row.song}</TableCell>
+                  <TableCell align="right">{row.artist}</TableCell>
+                  <TableCell align="right">{row.time}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="8vh"
+        ></Box>
+        <h1 className={styles.title}>3 Fav Artists from y'all 🎸</h1>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="8vh"
+        ></Box>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 850 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Ranking</TableCell>
+                <TableCell align="right">Artist</TableCell>
+                <TableCell align="right">Time Listened</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rowsArt.map((row) => (
+                <TableRow
+                  key={row.rank}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.rank}
+                  </TableCell>
+                  <TableCell align="right">{row.artist}</TableCell>
+                  <TableCell align="right">{row.time}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
